1548ff110b8278170199ad2b56b3b56874534074
Nick Cooper <nickcooper84@gmail.com>
2019-04-17 16:22:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 var a = Math.floor(Math.random() * 1000), b = Math.floor(Math.random() * 1000);
 
 function add(a,b) {
-return a + b
+return a + b;
 }
 
 function subtract(a,b) {
-return a - b
+return a - b;
 }
 
 function multiply(a,b) {
-return a * b
+return a * b;
 }
 
 function divide(a,b) {
-return a / b
+return a / b;
 }
 
-function makeInt(n) {
+function inc(n) {
+  n += 1;
+}
 
+function dec(n) {
+  n -= 1;
+} 
+
+function makeInt(n) {
+  return parseInt(n, 10);
 }
 
 function preserveDecimal(n) {
-
+  return parseFloat(n);
 }
+
+
