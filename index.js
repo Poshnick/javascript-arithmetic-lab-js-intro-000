3da765d4d74bba756d6a63ae57a2018fa769b941
Nick Cooper <nickcooper84@gmail.com>
2019-04-17 16:16:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var a = Math.floor(Math.random() * 1000), b = Math.floor(Math.random() * 1000);
 
-function add() {
+function add(a,b) {
 return a + b
 }
 
-function subtract() {
+function subtract(a,b) {
 return a - b
 }
 
-function multiply() {
+function multiply(a,b) {
 return a * b
 }
 
-function divide() {
+function divide(a,b) {
 return a / b
 }
 
