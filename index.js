4f97041f4c787e6e24df3b399f945c9a75e54f16
Nick Cooper <nickcooper84@gmail.com>
2019-04-17 16:13:22 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+var a = 5, b = 653;
+
+function add() {
+return a + b
+}
+
+function subtract() {
+return a - b
+}
+
+function multiply() {
+return a * b
+}
+
+function divide() {
+return a / b
+}
+
+function makeInt(n) {
+
+}
+
+function preserveDecimal(n) {
+
+}
